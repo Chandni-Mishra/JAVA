c78f411c5cf8edd144378fba9ecf8375a50c335a
Chandni-Mishra <91174476+Chandni-Mishra@users.noreply.github.com>
2022-03-30 16:50:11 +0530

    Create Stringbuffer.java

---
+++ Lab/Stringbuffer.java
@@ -0,0 +1,18 @@
+//String Buffer
+class Xyz {
+  public static void main(String args[])
+ {
+    StringBuffer s = new StringBuffer ("Chandni");
+    //s.append("Mishra");
+    //System.out.println(s);
+    //s.insert(2,"Hello");
+    //System.out.println(s);
+//s.replace(1,3,"Ei");
+//System.out.println(s);
+//s.delete(1,6);
+//System.out.println(s);
+s.reverse();
+System.out.println(s);
+}
+}
+    
