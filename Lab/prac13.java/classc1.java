ad96419fcf60505a56ee2519eefc3783a2647569
Chandni-Mishra <91174476+Chandni-Mishra@users.noreply.github.com>
2022-06-05 01:36:50 +0530

    Create classc1.java

---
+++ Lab/prac13.java/classc1.java
@@ -0,0 +1,13 @@
+package p1;
+public class C1{
+   public int sum(int a,int b)
+   {
+      int s = a+b;
+      return s;
+   }
+   public float div(float a,float b)
+   {
+       float r = a/b;
+       return r;
+   }
+}
