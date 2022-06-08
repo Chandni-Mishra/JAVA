06764170178b3a98153573b4eb01e042e51623d6
Chandni-Mishra <91174476+Chandni-Mishra@users.noreply.github.com>
2022-06-08 16:32:45 +0530

    Create database2.java

---
+++ Lab/DATABASE/database2.java
@@ -0,0 +1,36 @@
+//to establish connection
+//to insert data in database
+import java.sql.*;
+class A{
+ public static void main(String args[])
+{
+  Connection con = null;
+  try
+{
+  Class.forName("com.mysql.jdbc.Driver");
+  System.out.println("Sucess");
+  }
+  catch(Exception e) 
+{
+ System.out.println(e);
+}
+ try
+{
+ con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cse?autoReconnect=true&useSSL=false","root","gehu");
+System.out.println("Connection established");
+}
+ catch(Exception e)
+{
+ System.out.println("connection not established");
+}
+try 
+{
+Statement st = con.createStatement();
+st.executeUpdate("insert into emp (id,name)values(15,'CHANDNI')");
+}
+  catch(Exception e)
+{
+System.out.println("Statement related issues");
+ }
+}
+}
